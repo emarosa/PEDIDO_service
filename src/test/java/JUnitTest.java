1a5acfd045ebb90129373cd2a410b3ff4c9080bb
Julio Melo <julio.melo@cin.ufpe.br>
2017-12-04 13:58:10 -0300

    JUnit - Pedido

--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -1,4 +1,29 @@
+import java.util.*;
+import java.util.function.ToIntFunction;
 
+import org.junit.*;
+
+import br.com.fish.devops.domain.pedido.Pedido;
+
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.both;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.everyItem;
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 public class JUnitTest {
-
+ 
 }
